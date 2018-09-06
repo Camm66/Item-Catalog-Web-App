c513383ec19499654b09689314a6286e51f3ce81
Cameron Morales <cam.morales93@gmail.com>
2018-09-05 23:57:33 -0500

    feat: CSRF protection
    Added csrf_protect() to create hidden csrf tokens on the session.
    This needs further testing once Authentication is completed!

--- project.py
+++ project.py
@@ -12,6 +12,20 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+#Added CSRF protection
+@app.before_request
+def csrf_protect():
+    if request.method == "POST":
+        token = session.pop('_csrf_token', None)
+        if not token or token != request.form.get('_csrf_token'):
+            abort(403)
+
+def generate_csrf_token():
+    if '_csrf_token' not in session:
+        session['_csrf_token'] = some_random_string()
+    return session['_csrf_token']
+
+app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
 #JSON APIs to view Catalog Information
 @app.route('/JSON')
